313f25f704f4f011a55bbe3af9e0359540ae8b1f
bhpratt <bhpratt@ncsu.edu>
2023-05-31 15:57:21 -0400

    remove module_variable_optional_attrs experiment support

--- ez_vpc_cluster/versions.tf
+++ ez_vpc_cluster/versions.tf
@@ -9,8 +9,7 @@ terraform {
       version = "~>1.38.2"
     }
   }
-  required_version = ">=1.0"
-  experiments      = [module_variable_optional_attrs]
+  required_version = ">=1.3"
 }
 
 ##############################################################################
